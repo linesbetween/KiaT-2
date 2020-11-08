3b627cc090113acf57703072b07f4c2cc7752419
lines <linesbetween@users.noreply.github.com>
2020-11-08 07:00:47 -0500

    can't parse response to js object

--- public/js/app.js
+++ public/js/app.js
@@ -11,21 +11,13 @@ const uploadFile = (targetImage, arImage) => {
     const request = new XMLHttpRequest();
     const formData = new FormData();
     
+    var newMessage;
+
     request.open("POST", API_ENDPOINT, true);
     request.onreadystatechange = () => {
         if (request.readyState === 4 && request.status === 200) {
-        console.log(request.responseText);
-        }
-    };
-    formData.append("key", "small-fog-3905");
-    formData.append("email", "majiatester@gmail.com");
-    formData.append("target_type", 0);
-    formData.append("hologram_type", 1);
-    formData.append("file_image", targetImage);
-    formData.append("file_image_hologram", arImage);
-    var newMessage = request.send(formData);
-
-
+            console.log("what's breaking JSON" + request.responseType.toString());
+            newMessage = request.response; // need to store this newMessage as js object
     // save returned response to server
     // POST /message
     $.ajax({
@@ -41,3 +33,28 @@ const uploadFile = (targetImage, arImage) => {
         }
     });
     };
+    };
+    formData.append("key", "small-fog-3905");
+    formData.append("email", "majiatester@gmail.com");
+    formData.append("target_type", 0);
+    formData.append("hologram_type", 1);
+    formData.append("file_image", targetImage);
+    formData.append("file_image_hologram", arImage);
+    request.send(formData);
+
+    // save returned response to server
+    // POST /message
+    // $.ajax({
+    //     method: 'POST',
+    //     url: 'http://localhost:3000/message',
+    //     data: newMessage
+    // }).then(function(response) {
+    //     if (response.success) {
+    //         console.log("message saved");
+    //         alert("Your message is saved on EchoAR cloud!");
+    //     } else {
+    //         alert(response.message);
+    //     }
+    // });
+    // };
+}
